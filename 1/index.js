1085b2663d754c8dd874e0394b660286f814b7e9
MmmDelicious <78604079+MmmDelicious@users.noreply.github.com>
2022-02-20 14:09:47 +0400

    Add files via upload

--- 1/index.js
+++ 1/index.js
@@ -1,3 +1,16 @@
+createMatrix = (r,c) => {
+    let matrix = []
+    for(let i = 0; i < r; i++)
+    matrix[i] = []
+        
+
+    for(let i = 0; i < r; i++)
+        for(let j = 0; j<c; j++){
+            matrix[i][j] = 0
+    }
+   
+    return matrix
+}
 
 
 
@@ -57,11 +70,115 @@ function mult(matrix1, matrix2) {
         }
     }
 
-    return matrix = multMatrix
+    return  multMatrix
 }
 
 
-function holetsky(matrix) {
+function minus(matrix1,matrix2){
+    let minusMatrix = createMatrix(matrix1.length,matrix1[0].length)
+
+    for (let i = 0; i < matrix1.length; i++) { 
+        for (let j = 0; j < matrix2[0].length; j++) { 
+            minusMatrix[i][j] = +(matrix1[i][j] - matrix2[i][j]).toFixed(2)
+        }
+    }
+    return minusMatrix
+}
+function plus(matrix1,matrix2){
+    let minusMatrix = createMatrix(matrix1.length,matrix1[0].length)
+
+    for (let i = 0; i < matrix1.length; i++) { 
+        for (let j = 0; j < matrix2[0].length; j++) { 
+            minusMatrix[i][j] = +(matrix1[i][j] + matrix2[i][j]).toFixed(2)
+        }
+    }
+    return minusMatrix
+}
+function solve(matrix,stolbik){
+     
+    let multMatrix = (mult(transpose(matrix),matrix))
+    let finalMatrix = createMatrix(matrix.length,matrix[0].length)
+
+    finalMatrix[0][0] = +(Math.sqrt(multMatrix[0][0])).toFixed(2)
+   
+    for (let i = 0; i < matrix.length; i++) {
+        for (let j = 0; j < matrix[0].length; j++) {
+            if(i === 0 && j != 0){
+                finalMatrix[i][j] = +(multMatrix[0][j] / finalMatrix[0][0]).toFixed(2)
+            }
+            else if(i === j && i != 0){
+                let sum = 0
+                for(let k = 0; k < i; k++){
+                    sum += Math.pow(finalMatrix[k][i],2)
+                }
+                finalMatrix[i][j] = +(Math.sqrt(multMatrix[i][j] - sum)).toFixed(2)
+            } 
+            else if(j > i) {
+                let sum = 0
+                for(let k = 0; k < i; k++){
+                    sum += finalMatrix[k][i]*finalMatrix[k][j] 
+                }
+                finalMatrix[i][j] = +((multMatrix[i][j] - sum)/finalMatrix[i][i]).toFixed(2)
+            }
+        }
+    }
+
+    console.log(finalMatrix)
+        let x = createMatrix(stolbik.length,1)
+        let y = createMatrix(stolbik.length,1)
+
+      let finalStolbik = (mult(transpose(matrix),stolbik))
+      y[0][0] = +(finalStolbik[0][0] / finalMatrix[0][0]).toFixed(2)
+
+      for (let i = 1; i < finalStolbik.length; i++) {
+          let sum = 0
+         for(let k = 0; k < i;k++){
+
+            sum += finalMatrix[k][i]*y[k][0]
+         }
+         console.log(finalMatrix[i][i])
+         y[i][0] = +((finalStolbik[i][0]- sum) / finalMatrix[i][i]).toFixed(2)
+        
+    }
+
+        x[x.length - 1][0] = +(y[y.length - 1][0] / finalMatrix[finalMatrix.length - 1][finalMatrix.length - 1]).toFixed(2)
+
+
+        for(let i = finalStolbik.length - 2; i >= 0; i--){
+            let sum = 0;
+            for(let k = i + 1; k <= finalStolbik.length - 1; k++){
+                sum += finalMatrix[i][k]*x[k][0]
+            }
+            x[i][0] = +((y[i][0] - sum) / finalMatrix[i][i]).toFixed(2)
+        }
+
+
+
+
+
+
+
+
+
+
+        
+        console.log(finalMatrix)
+        console.log(y)
+        console.log(x)
+
+        console.log(finalStolbik)
+
+
+    
+
+
+
+     return {
+         x,
+         y
+     }
+}
+function holetsky(matrix,finalMatrix) {
 
     let coefficientP = []
     let coefficientC = []
@@ -111,7 +228,7 @@ function holetsky(matrix) {
 
 
 
-    let finalMatrix = [[2, 5, 13]]
+   
     let x = [], y = []
     for (let i = 0; i < finalMatrix[0].length; i++) {
         x[i] = y[i] = 0
@@ -130,7 +247,7 @@ function holetsky(matrix) {
     let n = matrix.length - 1;
 
     x[n] = y[n];
-    console.log(n)
+ 
     for (let i = n - 1; i >= 0; i--) {
         let sum = 0;
         for (let k = i + 1; k <= n; k++) {
@@ -138,27 +255,103 @@ function holetsky(matrix) {
         }
         x[i] = +(y[i] - sum).toFixed(2);
     }
-    console.log(x)
-    console.log(y)
-    console.log(coefficientP)
-    console.log(coefficientC)
-    matrixOut(coefficientP)
-    matrixOut(coefficientC)
-    matrixOut(x)
-    matrixOut(y)
+
+    return {
+        x,
+        y
+    }
+ 
+}
+
+function myMax(matrix){
+    let arr = []
+    let sum
+    for(let i = 0; i < matrix.length; i++){
+        sum = 0
+        for(let j = 0; j < matrix[0].length;j++){
+            sum += Math.abs(matrix[i][j])
+        } 
+        arr.push(sum)
+    }
+
+    return Math.max(...arr)
+}
+
+function yakobi(matrix,stolbik,eps){
+
+    for(let i = 0; i < matrix.length; i++){
+        stolbik[i][0] /= matrix[i][i]
+        for(let j = 0; j < matrix[0].length;j++){
+            if(i!=j){
+           
+            matrix[i][j] /= -matrix[i][i]
+             
+            }
+        } 
+    }
+    for(let i = 0; i < matrix.length; i++){
+        
+        for(let j = 0; j < matrix[0].length;j++){
+            if(i==j)
+            matrix[i][j] = 0
+             
+            
+        } 
+    }
+    console.log(stolbik)
+    
+    console.log(matrix)
+    let max = myMax(matrix)
+    let end = (max / (1 - max))*eps
+    console.log(max)
+    console.log(end)
+    let startStolbik = createMatrix(stolbik.length,stolbik[0].length)
+    let iterator = 0;
+    while(max > end && iterator < 16){
+       
+    let currentStolbik = plus(mult(matrix,startStolbik),stolbik)
+    max = +(myMax(minus(currentStolbik,startStolbik))).toFixed(2)
+    startStolbik = currentStolbik
+    
+    iterator++
+    console.log(max)
+    }
+    console.log(startStolbik)
+    console.log(matrix)
+    console.log(stolbik)
+
+
+    return startStolbik
 }
 
 
 
 
 
-let matrix = [[-4, 1, 1], [1, -9, 3], [1, 2, -16]]
-matrixOut(matrix)
-let transposeMatrix = transpose(matrix)
-matrixOut(transposeMatrix)
 
-// matrixOut(matrix)
-// mult(transposeMatrix,matrix)
-// matrixOut(matrix)
-holetsky(matrix)
+ let matrix = [[-4, 1, 1], [1, -9, 3], [1, 2, -16]]
+ let stolbik = [[2],[5],[13]]
 
+//let matrix = [[37, 2, 1.25,1,2], [4, 138, 1,0.5,1], [1,2,-49,3,4],[1,0.4,4,58,4],[-1,2,3,8,-69]]
+//let stolbik = [[1],[2],[3],[4],[5]]
+// let invalid = [[1,2,3,4,5]]
+
+
+
+
+//Для Якоби
+//matrixOut(matrix)
+// let eps = 0.02
+//matrixOut(yakobi(matrix,stolbik,eps))
+
+
+//Квадратный корень
+// matrixOut(solve(matrix,stolbik).x)
+// matrixOut(solve(matrix,stolbik).y)
+ 
+
+
+
+//Холецкий
+//   matrixOut(holetsky(matrix,invalid).x)
+//   matrixOut(holetsky(matrix,invalid).y)
